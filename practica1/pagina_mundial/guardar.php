6f89a79e619f3afcf1c7f413ef91f6561ff2bad5
dbalcazarr <DavidBalcazar@MacBook-Pro-de-David.local>
2014-06-27 16:17:58 -0500

    Primer commit

---
+++ practica1/pagina_mundial/guardar.php
@@ -0,0 +1,22 @@
+<?php
+echo "hola.....";
+echo "<br />";
+echo "Goles Equipo 1 Local:" .$_POST['equipo1_local'];
+echo "<br />";
+echo "Goles Equipo 2 Visitante:" .$_POST['equipo2_visitante'];
+
+echo "<br />";
+echo "Goles Equipo 3 Local:" .$_POST['equipo3_local'];
+echo "<br />";
+echo "Goles Equipo 4 Visitante:" .$_POST['equipo4_visitante'];
+
+echo "<br />";
+echo "Equipo elegido 1:" .$_POST['equipos_local'];
+echo "<br />";
+echo "Goles Equipo 5 Local:" .$_POST['equipo5_local'];
+echo "<br />";
+echo "Equipo Elegido 2:" .$_POST['equipos2_visitante'];
+echo "<br />";
+echo "Goles Equipo 6 visitante:" .$_POST['equipo6_visitante'];
+
+?>
